f303b51d022eb7edc003f62b8041744206ab7263
Nichole Axel <nfrancis22@gmail.com>
2022-05-24 15:32:33 -0400

    inital commit

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Counter from './src/components/Counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Fisher Price 'My First Mobile App'!</Text>
+      <Button title="Press me" />
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading:{
+    fontSize: 26,
+    color: '#405566'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#97d7d7',
     alignItems: 'center',
     justifyContent: 'center',
   },
